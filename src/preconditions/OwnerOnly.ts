9ea6d5939bafacb02f3f2b6418a3d6ae643a62dc
bitomic <35199700+bitomic@users.noreply.github.com>
2022-08-07 18:39:23 -0600

    fix: owner only precondition for chat input commands

--- src/preconditions/OwnerOnly.ts
+++ src/preconditions/OwnerOnly.ts
@@ -1,14 +1,20 @@
+import type { Interaction, Message } from 'discord.js'
 import type { PreconditionOptions, PreconditionResult } from '@sapphire/framework'
 import { ApplyOptions } from '@sapphire/decorators'
 import { env } from '../lib'
-import type { Message } from 'discord.js'
 import { Precondition } from '@sapphire/framework'
 
 @ApplyOptions<PreconditionOptions>( {
 	name: 'OwnerOnly'
 } )
 export class UserPrecondition extends Precondition {
-	public run( message: Message ): PreconditionResult {
+	public override chatInputRun( interaction: Interaction ): PreconditionResult {
+		return interaction.user.id === env.DISCORD_OWNER
+			? this.ok()
+			: this.error()
+	}
+
+	public override messageRun( message: Message ): PreconditionResult {
 		return message.author.id === env.DISCORD_OWNER
 			? this.ok()
 			: this.error()
